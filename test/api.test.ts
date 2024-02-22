ffc962673a073ffaa324b29873d6595689b4598a
gustcomer <gustcomer@gmail.com>
2024-02-22 19:06:52 -0300

    test: add close ticket route

--- test/api.test.ts
+++ test/api.test.ts
@@ -21,7 +21,7 @@ test("Must create a new ticket", async function() {
 test("Must assign the ticket to an attendee", async function() {
 	const input1 = {
 		requesterId: "b7e6db47-3e5d-4220-9265-0e6eef8c4330",
-		content: "A internet está muito lenta"
+		content: "The internet is not working"
 	};
   const response1 = await axios.post("http://localhost:3000/tickets", input1);
   const output1 = response1.data;
@@ -37,3 +37,30 @@ test("Must assign the ticket to an attendee", async function() {
 	expect(output3.assigneeId).toBe(input2.assigneeId);
 	expect(output3.status).toBe("assigned");
 })
+
+test("Must close the ticket", async function () {
+	const input1 = {
+		requesterId: "b7e6db47-3e5d-4220-9265-0e6eef8c4330",
+		content: "The internet is not working"
+	};
+	const response1 = await axios.post("http://localhost:3000/tickets", input1);
+	const output1 = response1.data;
+
+	const input2 = {
+		assigneeId: "d88aa636-22f5-4da1-9384-077f80e51301",
+		status: "assigned"
+	};
+	await axios.post(`http://localhost:3000/tickets/${output1.ticketId}/assign`, input2);
+
+	const input3 = {
+		status: "closed"
+	};
+	await axios.post(`http://localhost:3000/tickets/${output1.ticketId}/close`, input3);
+
+	const response4 = await axios.get(`http://localhost:3000/tickets/${output1.ticketId}`);
+	const output4 = response4.data;
+	console.log(output4);
+	expect(output4.endDate).toBeDefined();
+	expect(output4.duration).toBeDefined();
+	expect(output4.status).toBe("closed");
+});
